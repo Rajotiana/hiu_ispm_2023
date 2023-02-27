2109f2d66f1178ebc09b7a14fc3b8b687e4d0128
TsitoUw <tsitohainanirinarak2@gmail.com>
2023-02-27 15:39:56 +0300

    home page done

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,30 @@
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import sary from "../assets/orientation.jpg";
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div className="w-full flex flex-col items-center justify-center mt-5">
+      <div className="p-4 py-10 w-10/12">
+        <div className="text-center">Welcome to our career assessment tool! Answer a set of questions and discover the field that best matches your skills and interests.</div>
+      </div>
+      <div className="w-10/12 flex ">
+        <div className="h-64 border-2 shadow-xl border-sky-600 rounded-md flex justify-around p-8">
+          <div className="flex flex-col w-8/12 justify-around">
+            <div className="text-6xl text-sky-600 font-medium">Free orientation test</div>
+            <div className="w-full flex justify-start">
+              <Button variant="contained">
+                <Link to="/orientation">Take a test</Link>
+              </Button>
+            </div>
+          </div>
+          <div className="w-4/12 flex justify-center items-center">
+            <img src={sary} className="w-1/2 h-auto" />
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  );
 };
 
 export default Home;
